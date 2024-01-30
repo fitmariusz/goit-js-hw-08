62ba14c50ad620a8893283e74387b4e3e2f7e3b9
fitmariusz <fitmariusz@gmail.com>
2024-01-31 00:49:11 +0100

    finish  02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import _ from 'lodash'
 
+const player = new Player("vimeo-player", {
+    id: 19231868,
+    width: 640,
+});
+
+lastTime = localStorage.getItem("videoplayer-current-time")
+player.setCurrentTime(lastTime);
+
+player.on('timeupdate', _.throttle((actualTime) => {
+    localStorage.setItem("videoplayer-current-time", actualTime.seconds);
+}, 1000));
